b1b68a626672b0a7aa6db377ea815a6529d96d86
Destan Tezel <destantezel@googlemail.com>
2023-05-22 17:11:04 +0000

    Add get_stock_values function and return dict for stock needed in the next market day

--- run.py
+++ run.py
@@ -118,6 +118,19 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    Returns dictionary for stock needed for the next market day
+    """
+    headings = SHEET.worksheet('stock').row_values(1)
+
+    dict = {}
+    for i in range(len(headings)):
+        dict[headings[i]] = data[i]
+
+    return dict
+
+
 def main():
     """
     Run all program functions
@@ -130,6 +143,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    stock_values = get_stock_values(stock_data)
+    
 
 
 print('Welcome to Love Sandwiches Data Automation')
